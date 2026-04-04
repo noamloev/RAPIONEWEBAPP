5333175c57cf598172b4c7244f45de33ce6b8356
unknown <noamloev@gmail.com>
2026-04-04 22:05:19 +0300

    adding url of server download

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -30,7 +30,7 @@ export function AppHeader({ title }: { title: string }) {
   }, []);
 
   function downloadLocalServer() {
-    window.open("https://your-server.com/download-local-server", "_blank");
+    window.open("https://github.com/noamloev/ANUCHKA-RELEASES/releases/download/v1.0.0/localserver.zip", "_blank");
   }
 
   return (
