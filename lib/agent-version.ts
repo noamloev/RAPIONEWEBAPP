988cfeaf2700ddcc46645ca92d18daadccb07541
unknown <noamloev@gmail.com>
2026-04-13 10:43:15 +0300

    new local server update

--- lib/agent-version.ts
+++ lib/agent-version.ts
@@ -1,4 +1,4 @@
 export const LATEST_AGENT_VERSION = "2.0.1";
 
 export const AGENT_INSTALLER_URL =
-  "https://github.com/noamloev/ANUCHKA-RELEASES/releases/download/v2.0.0/Loevflowlocalserver-1.0.0-setup.exe";
+  "https://github.com/noamloev/ANUCHKA-RELEASES/releases/download/v2.0.1/Loevflowlocalserver-2.0.1-setup.exe";
